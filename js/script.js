939667737454a28b0775c42cc824f132ed72d2f5
Julia <julia.kocik02@gmail.com>
2021-02-03 12:29:13 +0100

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+function playGame (playerInput) {
+clearMessages();
 // computerMove
 function getMoveName(argMoveId){
     if(argMoveId == 1){
@@ -11,41 +13,18 @@ function getMoveName(argMoveId){
       return 'nieznany ruch';
     }
   }
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
 
-/*
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier'
-} else if (randomNumber == 3) {
-    computerMove = 'nożyce'
-}
-*/
-
 printMessage('Mój ruch to: ' + computerMove);
 
 //playerMove
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = getMoveName(playerInput);
-
-/*
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if(playerInput == '2') {
-    playerMove = 'papier';
-} else if (playerInput == '3') {
-    playerMove = 'nożyce';
-}
-*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
@@ -71,3 +50,16 @@ function displayResult(argComputerMove, argPlayerMove){
 }
 
 displayResult(computerMove, playerMove);
+}
+
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+  });
+
+  document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+  });
+
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });
